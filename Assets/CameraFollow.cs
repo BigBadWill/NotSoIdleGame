c845838671520525403f0f7bef2ae073bb97ba86
BigBadWill <doesnotcarebear@gmail.com>
2019-12-13 17:05:22 +1000

    Movement/Camera
    Added a very shitty movement and barely functional camera program. Both need to be worked on.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform camtarget;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        float movHoriz = Input.GetAxis("Mouse X");
+        transform.Rotate(0, movHoriz, 0);
+        Vector3 desirepos = camtarget.position + offset;
+        Vector3 smoothpos = Vector3.SmoothDamp(transform.position, desirepos, ref velocity, smoothSpeed);
+        transform.position = smoothpos;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myhotsexybody;
+
+    public float speed;
+    
+    void Start()
+    {
+        myhotsexybody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float HorizMov = Input.GetAxis("Horizontal");
+        float VertMov = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(HorizMov, 0.0f, VertMov);
+
+        myhotsexybody.AddForce(movement * speed);
+    }
+}
